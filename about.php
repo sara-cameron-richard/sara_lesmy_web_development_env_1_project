524bc842af773fd286d3132ba09acd79896bd2d1
sara-cameron-richard <134742947+sara-cameron-richard@users.noreply.github.com>
2023-06-08 11:26:48 -0400

    We updated the code.
    Added index.php, about.php, an asset folder with images and css styling.

---
+++ about.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="assets/css/about.css">
+</head>
+<body>
+    <header>
+        <nav>              
+            <a href="index.php">home</a>
+            <a href="about.php">about us</a>
+        </nav>
+    </header>
+    <section class="about-sara">
+        <div class="container">
+            <h2>We are a Work in Progress</h2>
+            <div class="sara-flex">
+                <div class="left-s"></div>
+                <div class="right-s">
+                <p>I'm starting a new career as a website specialist after a long career taking photos and teaching visual communication methods in geography departments across the city.</p>
+                <p class="name">-- Sara Cameron</p>
+                <a class="icon-link" href="https://github.com/sara-cameron-richard" target="_blank"> <i class="fa-brands fa-github"></i>Sara's GitHub</a>
+                </div>
+            </div>
+        </div>
+    </section> 
+    <section class="about-lesmy">
+        <div class="container">
+            <div class="lesmy-flex">
+                <div class="left-l">
+                    <p>I'm a Graphic Designer and I worked in Marketing for over 20 years. I'm passionate about web deign and I love to create beautiful desing websites.</p>
+                    <p class="name">-- Lesmy Perez</p>
+                    <a class="icon-link" href="https://github.com/LesmyP" target="_blank"> <i class="fa-brands fa-github"></i>Lesmy's GitHub</a>
+                </div>
+                <div class="right-l"></div>
+            </div>
+        </div>
+    </section> 
+
+
+    <footer>This website is under construction, thank you for your patience</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final Project Web Development Enviroment</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/home.css">
+   </head>
+<body>
+    <header>
+        <nav>              
+            <a href="index.php">home</a>
+            <a href="about.php">about us</a>
+        </nav>
+    </header>
+
+
+     <section class="bigbanner">
+        <div class="textbox">
+            <h2>Web Developing Environement 1</h2>
+            <h1>Final Project</h1>
+            <p>by Sara Cameron and Lesmy Perez</p>
+        </div>
+    </section>
+
+    <section class="testimonials">
+        <div class="container">
+            <h3>Vanier - Website Design Specialist</h3>
+            <div class="testimonialflex">
+                <div class="left"></div>
+                <div class="right">
+                <p>Special thanks to Jeevesh for always be willing to answer our questions and help us to improve our work</p>
+                <p class="name">-- Lesmy and Sara</p>
+                </div>
+            </div>
+        </div>
+    </section> 
+
+    <footer>This website is under construction, thank you for your patience</footer>
+
+</body>
+</html>
